dc266b199d0558b090cc2417e8461121cf0aded9
Robert <robert1.nelson@umconnect.umt.edu>
2022-12-04 22:36:57 -0700

    new file:   .gitignore 	new file:   Pre Payment Program.code-workspace 	new file:   index.html 	new file:   jsconfig.json 	new file:   main.css 	new file:   main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// document.getElementById("button").addEventListener("click", calculate);
+
+// function calculate() {
+//   const valueOfLoan = Number(document.getElementById("loanValue").value);
+//   const yearsOfLoan = Number(document.getElementById("loanage").value);
+//   const returnElement = document.getElementById("returnParagraph");
+
+//   console.log(valueOfLoan);
+
+//   if (yearsOfLoan === 1) {
+//     returnElement.innerText = valueOfLoan * 0.05;
+//   } else if (yearsOfLoan === 2) {
+//     returnElement.innerText = valueOfLoan * 0.04;
+//   } else if (yearsOfLoan === 3) {
+//     returnElement.innerText = valueOfLoan * 0.03;
+//   } else if (yearsOfLoan === 4) {
+//     returnElement.innerText = valueOfLoan * 0.02;
+//   } else {
+//     returnElement.innerText = valueOfLoan * 0.01;
+//   }
+// }
+
+// if( i <= 3.5 && p >=(l * .02)) continue;
+// if()
+
+// Interest rate variable, prepayment amount variable, if the prepayment amount is
+//greater or equal to 20% of current loan amount then there is a penalty if not there is no penalty.
+//If interest rate is 3% or greater execute prepayment. also add some css to make it prettier. -->
+
+// NOTES**************
+// DOES THE FUNCTION NEED TO BE PLACED BEFORE THE BUTTON OR AFTER?
+// DO WE NEED TO SCRIPT SRC THE MAIN.JS FOLDER?
+// POTENTIAL OUTPUT TAG
+// WHERE DO WE CALL THE FUNCTION?
+// Display on screen
+// look more into calling js code into html, writing it in html, pulling inputs from html to js code and the output it in html
+// make html input type and int instead of number
+// push js code outputs into an array and then tie those to an output paragraph!!
+
+// instead of creating fuction for two numbers, create another function after its been calculated take i and p output and output you dont have to pay
